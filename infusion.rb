0de469f5121668e54860683ac277cf1371ed573e
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-11-21 00:29:46 +1300

    Improved the build configuration.

--- infusion.rb
+++ infusion.rb
@@ -3,38 +3,38 @@
 #  This file is part of the "Teapot" project, and is released under the MIT license.
 #
 
-required_version "0.1"
+required_version "0.2"
 
 define_platform "darwin-ios" do |platform|
 	platform.configure do
-		xcode_path Pathname.new(`xcode-select --print-path`.chomp)
-		platform_path {xcode_path + "Platforms/iPhoneOS.platform"}
-		toolchain_path {xcode_path + "Toolchains/XcodeDefault.xctoolchain"}
+		default xcode_path Pathname.new(`xcode-select --print-path`.chomp)
+		default platform_path {xcode_path + "Platforms/iPhoneOS.platform"}
+		default toolchain_path {xcode_path + "Toolchains/XcodeDefault.xctoolchain"}
 		
 		default sdk_version {ENV["IPHONE_SDK_VERSION"] || "6.0"}
-		sdk_path {platform_path + "Developer/SDKs/iPhoneOS#{sdk_version}.sdk"}
+		default sdk_path {platform_path + "Developer/SDKs/iPhoneOS#{sdk_version}.sdk"}
 
 		default architectures ["-arch armv7"]
 
-		buildflags {[
-			architectures,
-			"-isysroot", sdk_path,
-			"-miphoneos-version-min=#{sdk_version}",
-		]}
+		buildflags [
+			:architectures,
+			"-isysroot", :sdk_path,
+			->{"-miphoneos-version-min=#{sdk_version}"},
+		]
 
-		cflags {[
-			buildflags
-		]}
+		linkflags []
 
-		cxxflags {[
-			buildflags
-		]}
+		ccflags [:buildflags]
+		cxxflags [:buildflags]
+		ldflags [:buildflags, :linkflags]
 
 		configure ["--host=arm-apple-darwin"]
 
-		cc {toolchain_path + "usr/bin/clang"}
-		cxx {toolchain_path + "usr/bin/clang++"}
-		ld {toolchain_path + "usr/bin/ld"}
+		default cc {toolchain_path + "usr/bin/clang"}
+		default cxx {toolchain_path + "usr/bin/clang++"}
+		default ld {toolchain_path + "usr/bin/ld"}
+		default ar {toolchain_path + "usr/bin/ar"}
+		default libtool {toolchain_path + "usr/bin/libtool"}
 	end
 	
 	platform.make_available! if RUBY_PLATFORM.include?("darwin")
