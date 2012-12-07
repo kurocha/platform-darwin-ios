7d06bb5c61318784d8db69e18e568944e4cb9e36
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-12-07 13:54:01 +1300

    Update for dependency based builds.

--- infusion.rb
+++ infusion.rb
@@ -3,14 +3,16 @@
 #  This file is part of the "Teapot" project, and is released under the MIT license.
 #
 
-required_version "0.2"
+required_version "0.5"
 
-define_platform "darwin-ios" do |platform|
-	platform.configure do
+define_package "darwin-ios" do |package|
+	package.provides "System/darwin-ios" do
+		default platform_name "darwin-ios"
+
 		default xcode_path Pathname.new(`xcode-select --print-path`.chomp)
 		default platform_path {xcode_path + "Platforms/iPhoneOS.platform"}
 		default toolchain_path {xcode_path + "Toolchains/XcodeDefault.xctoolchain"}
-		
+
 		default sdk_version {ENV["IPHONE_SDK_VERSION"] || "6.0"}
 		default sdk_path {platform_path + "Developer/SDKs/iPhoneOS#{sdk_version}.sdk"}
 
@@ -20,6 +22,7 @@ define_platform "darwin-ios" do |platform|
 			:architectures,
 			"-isysroot", :sdk_path,
 			->{"-miphoneos-version-min=#{sdk_version}"},
+			"-pipe"
 		]
 
 		linkflags []
@@ -37,5 +40,28 @@ define_platform "darwin-ios" do |platform|
 		default libtool {toolchain_path + "usr/bin/libtool"}
 	end
 	
-	platform.make_available! if RUBY_PLATFORM.include?("darwin")
+	package.provides :system => ["System/darwin-ios"]
+	
+	package.provides 'Language/C++11' do
+		cxxflags %W{-std=c++11 -stdlib=libc++ -Wno-c++11-narrowing}
+	end
+	
+	package.provides 'Library/OpenGLES' do
+		ldflags ["-framework", "OpenGLES"]
+	end
+	
+	package.provides 'Aggregate/OpenGL' => "Library/OpenGLES"
+	
+	package.provides 'Library/OpenAL' do
+		ldflags ["-framework", "OpenAL"]
+	end
+	
+	package.provides 'Aggregate/UIKit' do
+		ldflags [
+			"-framework", "Foundation",
+			"-framework", "CoreVideo",
+			"-framework", "CoreServices",
+			"-framework", "UIKit"
+		]
+	end
 end
