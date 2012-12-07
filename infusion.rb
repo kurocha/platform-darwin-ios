2c83e5ca4af4fa3e4743e7bda9567122e41ffa8d
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-12-08 01:06:04 +1300

    Combined ios and ios-simulator packages. package -> target.

--- infusion.rb
+++ infusion.rb
@@ -5,8 +5,33 @@
 
 required_version "0.5"
 
-define_package "darwin-ios" do |package|
-	package.provides "System/darwin-ios" do
+def setup_provisions(target)
+	target.provides 'Language/C++11' do
+		cxxflags %W{-std=c++11 -stdlib=libc++ -Wno-c++11-narrowing}
+	end
+	
+	target.provides 'Library/OpenGLES' do
+		ldflags ["-framework", "OpenGLES"]
+	end
+	
+	target.provides 'Aggregate/OpenGL' => "Library/OpenGLES"
+	
+	target.provides 'Library/OpenAL' do
+		ldflags ["-framework", "OpenAL"]
+	end
+	
+	target.provides 'Aggregate/UIKit' do
+		ldflags [
+			"-framework", "Foundation",
+			"-framework", "CoreVideo",
+			"-framework", "CoreServices",
+			"-framework", "UIKit"
+		]
+	end
+end
+
+define_target "platform-darwin-ios" do |target|
+	target.provides "Platform/darwin-ios" do
 		default platform_name "darwin-ios"
 
 		default xcode_path Pathname.new(`xcode-select --print-path`.chomp)
@@ -40,28 +65,50 @@ define_package "darwin-ios" do |package|
 		default libtool {toolchain_path + "usr/bin/libtool"}
 	end
 	
-	package.provides :system => ["System/darwin-ios"]
+	target.depends :variant
 	
-	package.provides 'Language/C++11' do
-		cxxflags %W{-std=c++11 -stdlib=libc++ -Wno-c++11-narrowing}
-	end
+	target.provides :platform => ["Platform/darwin-ios"]
 	
-	package.provides 'Library/OpenGLES' do
-		ldflags ["-framework", "OpenGLES"]
-	end
-	
-	package.provides 'Aggregate/OpenGL' => "Library/OpenGLES"
-	
-	package.provides 'Library/OpenAL' do
-		ldflags ["-framework", "OpenAL"]
-	end
-	
-	package.provides 'Aggregate/UIKit' do
-		ldflags [
-			"-framework", "Foundation",
-			"-framework", "CoreVideo",
-			"-framework", "CoreServices",
-			"-framework", "UIKit"
+	setup_provisions(target)
+end
+
+define_target "platform-darwin-ios-simulator" do |target|
+	target.provides "Platform/darwin-ios-simulator" do
+		default platform_name "darwin-ios-simulator"
+
+		default xcode_path Pathname.new(`xcode-select --print-path`.chomp)
+		default platform_path {xcode_path + "Platforms/iPhoneSimulator.platform"}
+		default toolchain_path {xcode_path + "Toolchains/XcodeDefault.xctoolchain"}
+		
+		default sdk_version {ENV["IPHONE_SDK_VERSION"] || "6.0"}
+		default sdk_path {platform_path + "Developer/SDKs/iPhoneSimulator#{sdk_version}.sdk"}
+
+		default architectures %W{-arch i386}
+
+		buildflags [
+			:architectures,
+			"-isysroot", :sdk_path,
+			->{"-miphoneos-version-min=#{sdk_version}"},
 		]
+
+		linkflags []
+
+		cflags [:buildflags]
+		cxxflags [:buildflags]
+		ldflags [:buildflags, :linkflags]
+
+		configure []
+
+		default cc {toolchain_path + "usr/bin/clang"}
+		default cxx {toolchain_path + "usr/bin/clang++"}
+		default ld {toolchain_path + "usr/bin/ld"}
+		default ar {toolchain_path + "usr/bin/ar"}
+		default libtool {toolchain_path + "usr/bin/libtool"}
 	end
+	
+	target.depends :variant
+	
+	target.provides :platform => ["Platform/darwin-ios-simulator"]
+	
+	setup_provisions(target)
 end
